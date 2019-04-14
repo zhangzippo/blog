e204baea551311bfb0e8f562cdd0fe3faf4e6a87
zippo <zhangzhp3@163.com>
2019-04-14 16:18:49 +0800

    feat:加评论

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -82,14 +82,13 @@ module.exports = {
     ],
 
     // 评论配置，参考下方 [页面评论] 章节
-    // comments: {
-      
-    //   // owner: 'zippo',
-    //   // repo: 'vuepress-theme-meteorlxy',
-    //   // clientId: 'MY_CLIENT_ID',
-    //   // clientSecret: 'MY_CLIENT_SECRET',
-    //   // autoCreateIssue: process.env.NODE_ENV !== 'development',
-    // },
-    comments: false,
+    comments: {
+      platform: 'github',
+      owner: 'zhangZippo',
+      repo: 'zhangZippo.github.io',
+      clientId: '13ba6a84d27ba53bd7d1',
+      clientSecret: 'ac2697b271741c47c64244ea5ef4207bbf0e787a',
+      autoCreateIssue: process.env.NODE_ENV !== 'development',
+    },
   },
 }
